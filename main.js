89e6d586047c9d45167382e00d833939168c9caa
Biscolad <biscolad@gmail.com>
2023-10-28 16:44:11 -0400

    add event listener

---
+++ main.js
@@ -0,0 +1,5 @@
+//add an event listener for the form submission
+form.addEventListener("submit", function(event) {
+    //prevent the form from submitting automatically
+    event.preventDefault();
+})
