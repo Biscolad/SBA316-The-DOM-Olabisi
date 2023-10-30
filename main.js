cbdfc17f061e396fca5228566f4a858fe5a42910
Biscolad <biscolad@gmail.com>
2023-10-29 20:40:13 -0400

    selectelementbyID, textContent, event listener

--- main.js
+++ main.js
@@ -1,5 +1,46 @@
+//CACHE THE FORM ELEMENT
+const form = document.getElementById("form");
+
+//Access form element
+const formHeader = document.getElementById("form-header");
+formHeader.textContent = "Students Register Here";
+console.log(formHeader);
+
 //add an event listener for the form submission
 form.addEventListener("submit", function(event) {
-    //prevent the form from submitting automatically
+    //prevent the form from submitting automaticlly
     event.preventDefault();
 })
+
+
+//ADD EVENT LISTENER TO EMAIL INPUT
+    const emailInput = document.getElementById("email");
+    emailInput.addEventListener("input", function() {
+        const emailValue = emailInput.value;
+    //CREATE EVENT HANDLER FUNCTIONS
+    if  (isValidEmail(emailValue)) {
+        emailInput.style.border = "2px solid blue";
+        
+    } else {
+        emailInput.style.border = "1px solid red";
+    }
+    
+        console.log(emailValue);
+    })
+
+
+
+
+
+
+
+
+function validate(evt) {
+    constnameVal = validateName();
+    if (nameVal === false) {
+        evt.returnValue = false;
+        return false
+    }
+}
+
+
